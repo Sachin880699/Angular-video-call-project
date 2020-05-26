8cc73e43e7d52646b74e0b62d07557846abcde72
sachin pawar <sachin@localhost.localdomain>
2020-05-26 13:27:08 +0530

    Angular video call project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularAgoraRtcModule, AgoraConfig } from 'angular-agora-rtc'; // Add
+
+
+const agoraConfig: AgoraConfig = {
+  AppID: 'Enter here you id',
+};
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularAgoraRtcModule.forRoot(agoraConfig) // Add
   ],
   providers: [],
   bootstrap: [AppComponent]
